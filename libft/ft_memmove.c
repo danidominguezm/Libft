5630184980466cb350e099eb0b773ed8135e8aab
Daniel Dominguez martinez <ddomingu@c1r3s6.42madrid.com>
2021-03-02 21:37:30 +0100

    .

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ddomingu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 19:29:09 by ddomingu          #+#    #+#             */
-/*   Updated: 2021/03/02 20:54:24 by ddomingu         ###   ########.fr       */
+/*   Updated: 2021/03/02 21:36:47 by ddomingu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,25 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char *dst_tmp;
 	unsigned char *src_tmp;
-	unsigned char *aux;
-	size_t lenaux;
 
-	lenaux = len;
 	dst_tmp = (unsigned char *) dst;
-	aux = (unsigned char *) src;
 	src_tmp = (unsigned char *) src;	
 
 	if (!len || (!dst && !src))
 		return (dst);
-
-	while (lenaux--)
+	
+	if (src_tmp < dst_tmp)
 	{
-		*()
+		while (len--)
+		{	
+			*(dst_tmp + len) = *(src_tmp + len);
+		}
 	}
-	while (len--)
-	{	
-		*(dst_tmp + len) = *(src_tmp + len);
+
+	else 
+	{
+		ft_memcpy(dst, src, len);
 	}
+	
 	return (dst);
 }
