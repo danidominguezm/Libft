b482dc6d4b19c624e4268fb8f72f6690ed9cc132
Daniel Dominguez martinez <ddomingu@c1r2s1.42madrid.com>
2021-03-03 21:15:39 +0100

    .

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,35 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddomingu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/02 19:29:09 by ddomingu          #+#    #+#             */
-/*   Updated: 2021/03/03 21:11:27 by ddomingu         ###   ########.fr       */
+/*   Created: 2021/03/03 21:11:37 by ddomingu          #+#    #+#             */
+/*   Updated: 2021/03/03 21:15:02 by ddomingu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	size_t		len_tmp;
-	unsigned char		cpy[len];
-	unsigned char		*src_tmp;
-	unsigned char		*dst_tmp;
-
-	len_tmp = len;
-	src_tmp = (unsigned char *)src;
-	dst_tmp = (unsigned char *)dst;
-	if (!len || (!dst && !src))
-		return (dst);
-	
-	while (len_tmp--)
-		cpy[len_tmp] = *(src_tmp + len_tmp);	
-	
-	while (len--)
-		*(dst_tmp + len) = cpy[len];
-
-	return (dst);
-}
--- libft/libft.h
+++ libft/libft.h
@@ -29,4 +29,5 @@ void	*ft_memcpy(void *dst, const void *src, size_t len);
 void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
 #endif
