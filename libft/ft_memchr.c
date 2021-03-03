d5bdf26561d12ffe3c1cfa10844b15e7eb42c486
Daniel Dominguez martinez <ddomingu@c1r2s1.42madrid.com>
2021-03-03 21:41:26 +0100

    .

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,8 +6,23 @@
 /*   By: ddomingu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 21:11:37 by ddomingu          #+#    #+#             */
-/*   Updated: 2021/03/03 21:15:02 by ddomingu         ###   ########.fr       */
+/*   Updated: 2021/03/03 21:41:09 by ddomingu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void    *ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	unsigned char	*s_tmp;
+
+	s_tmp = (unsigned char *) s;
+
+	i = 0;
+	while (i++ < n)	
+			if ((char) c == *(s_tmp + i))
+				return ((void *)s + i);
+
+	return (NULL);
+}
