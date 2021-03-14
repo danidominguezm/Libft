0792f5325800dcfc83eef8b961b7703ab95be180
Daniel Dominguez martinez <ddomingu@c1r3s5.42madrid.com>
2021-03-14 20:38:54 +0100

    .

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,12 +6,16 @@
 /*   By: ddomingu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 19:22:04 by ddomingu          #+#    #+#             */
-/*   Updated: 2021/03/14 20:31:37 by ddomingu         ###   ########.fr       */
+/*   Updated: 2021/03/14 20:38:42 by ddomingu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_itoa(int n)
 {
+	char *str;
+
+	str = ft_calloc(ft_nbr_counter(n) + 1, sizeof(char));
+	
 	unsigned int nbr;
 
 	if (n < 0)
@@ -35,20 +39,16 @@ static int	ft_nbr_counter(int n)
 {
 	int i;
 
-	i = 0;
+	len = 0;
 	if (n == 0)
 		return (1);
 	else if (n < 0)
 	{
-		i++;
+		len++;
 		n *= -1;
 	}
-	while (n > 0 && ++i)
+	while (n > 0 && ++len)
 		n /= 10;
-	return (i);
+	return (len);
 }
-{
-	char *str;
 
-
-}
